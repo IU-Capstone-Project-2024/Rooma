beb120cf1a95bc0791dd77ea16b0aea175041154
darya <d.koncheva@innopolis.university>
2024-06-13 22:21:14 +0300

    Create frontend base

---
+++ frontend/components/business/ReactRouter.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ReactRouter extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+
+export default ReactRouter;
