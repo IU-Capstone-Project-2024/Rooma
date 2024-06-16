34098f741f7392d4193ff29163f07b86aa8ae080
bakinsa <bakinasa@gmail.com>
2024-06-16 16:22:05 +0300

    updated the project structure, refactored the code

--- frontend/src/components/game/GameCard.jsx
+++ frontend/src/components/game/GameCard.jsx
@@ -6,7 +6,7 @@ export default function GameCard({name, img, small, onClick}) {
             small ? "h-48 md:h-56 xl:h-64" : "h-56 md:h-64 xl:h-80")}
              style={{backgroundImage: `url(${img})`}} onClick={onClick}>
             <div className="text-center py-1 text-xl bg-[#] backdrop-brightness-75 text-white rounded-t-2xl">{name}</div>
-            <button className={classNames("m-2 bg-amber-500 self-start rounded-xl", small ? "px-6 py-1" : "px-8 py-2")}>Play</button>
+            <button className={classNames("m-2 bg-gradient-to-r from-yellow-400 to-pink-500 self-start rounded-xl text-white", small ? "px-6 py-1" : "px-8 py-2")}>Play</button>
         </div>
     );
 }
--- frontend/src/pages/index/Index.jsx
+++ frontend/src/pages/index/Index.jsx
@@ -1,9 +1,7 @@
-import GameCard from "../../components/game/GameCard.jsx";
-import hideAndSeek from "../../assets/hide_and_seek.png";
-import killer from "../../assets/killer.png";
-import space from "../../assets/space.png";
-import {useState} from "react";
-import classNames from "classnames";
+import hideAndSeek from "../../assets/GameCardAvatars/hide_and_seek.png";
+import killer from "../../assets/GameCardAvatars/killer.png";
+import space from "../../assets/GameCardAvatars/space.png";
+import {GamesCarousel} from "../../components/game/GamesCarousel.jsx";
 
 const cards = [
     {
@@ -28,36 +26,11 @@ const cards = [
     },
 ];
 
-function GamesCarousel() {
-    const [cardIndex, setCardIndex] = useState(1);
-
-    const prevIndex = cardIndex === 0 ? cards.length - 1 : cardIndex - 1;
-    const nextIndex = cardIndex === cards.length - 1 ? 0 : cardIndex + 1;
-
-    return (
-        <div className="flex flex-col gap-5">
-            <div className="flex justify-around items-center gap-5 overflow-x-hidden">
-                <GameCard name={cards[prevIndex].name} img={cards[prevIndex].img} small={true}
-                          onClick={() => setCardIndex(cardIndex === 0 ? cards.length - 1 : cardIndex - 1)}/>
-                <GameCard name={cards[cardIndex].name} img={cards[cardIndex].img}/>
-                <GameCard name={cards[nextIndex].name} img={cards[nextIndex].img} small={true}
-                          onClick={() => setCardIndex((cardIndex + 1) % cards.length)}/>
-            </div>
-            <div className="flex items-center gap-3 self-center">
-                {cards.map((_, index) => (
-                    <div key={index} className={classNames("cursor-pointer rounded-full h-3 w-3", cardIndex === index ? "bg-[#FFCC7A]" : "bg-[#C8C8C8]")}
-                         onClick={() => setCardIndex(index)}/>
-                ))}
-            </div>
-        </div>
-    );
-}
-
 export default function Index() {
 
     return (
         <section>
-            <GamesCarousel/>
+            <GamesCarousel cards={cards}/>
         </section>
     );
 }
