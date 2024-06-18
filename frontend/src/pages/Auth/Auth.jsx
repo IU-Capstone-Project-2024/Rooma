69812897a4e1cd96c5dcd309070eb1a26e313cd9
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-06-19 00:27:16 +0300

    Added Auth logic

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import {getTelegramId, getToken, setTelegramId, setToken} from '@/utils/storage.js';
+import App from "@/App.jsx";
 
-const Auth = () => {
-    return (
-        <>
 
-        </>
-    );
+const button_style = {
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: '#0088cc', // Telegram's primary color
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer'
 };
 
-export default Auth;
+
+export default function Auth() {
+    // TODO: add states etc. to prevent double execution
+
+    const url = window.location.search;
+    const params = new URLSearchParams(url);
+
+    const queryToken = params.get('token');
+    const queryTelegramId = params.get('telegram_id');
+
+    let token = getToken();
+    let telegramId = getTelegramId();
+
+    if ((!token || !telegramId) && (!queryToken || !queryTelegramId)) {
+        const telegramBotLink = 'https://t.me/whatis_test_bot?start=create_game';
+
+        return (
+            <a href={telegramBotLink} target="_blank" rel="noopener noreferrer">
+                <button style={button_style}>Authorize in Telegram Bot</button>
+            </a>
+        );
+    } else if (queryToken && queryTelegramId) {
+        console.log(1, queryToken, queryTelegramId, token, telegramId);
+
+        setToken(queryToken);
+        setTelegramId(queryTelegramId);
+    }
+
+    token = getToken();
+    telegramId = getTelegramId();
+
+    console.log(2, queryToken, queryTelegramId, token, telegramId);
+
+    return <App/>
+};
+
