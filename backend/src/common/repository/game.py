9269a492152dd03739dec031de085b0a57c2859e
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-06-19 02:34:00 +0300

    Added methods to repo

--- backend/src/common/repository/game.py
+++ backend/src/common/repository/game.py
@@ -1,6 +1,19 @@
-from src.database.models import Game
+from uuid import UUID
+
+from src.database.models import Game, User
 from src.database.repository.mongo import MongoBeanieRepository
 
 
 class GameRepository(MongoBeanieRepository):
     model = Game
+
+    async def get_one_by_game_id(self, game_id: UUID) -> Game | None:
+        return await self.model.find_one({"game_id": game_id})
+
+    async def add_user_to_lobby(self, game_id: UUID, user: User) -> None:
+        game = await self.get_one_by_game_id(game_id)
+        if not game:
+            return
+
+        game.lobby.append(user)
+        _ = await game.save()
