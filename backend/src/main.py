7f8668813b1bba1ffa8dd291334d62315d6b7247
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-06-19 01:27:17 +0300

    Added CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.bridge.router import router as bridge_router
 from src.database import init_db
@@ -23,3 +24,16 @@ app = FastAPI(lifespan=lifespan)
 
 # include routers
 app.include_router(bridge_router)
+
+# CORS
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
