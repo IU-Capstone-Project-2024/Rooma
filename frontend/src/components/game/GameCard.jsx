5237d5327cc4b4cf15fa416e271d3691cad13099
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-06-19 01:03:09 +0300

    Added settings popup placeholder

--- frontend/src/components/game/GameCard.jsx
+++ frontend/src/components/game/GameCard.jsx
@@ -1,12 +1,36 @@
 import classNames from "classnames";
+import Popup from "reactjs-popup";
+import 'reactjs-popup/dist/index.css';
 
 export default function GameCard({name, img, small, onClick}) {
     return (
-        <div className={classNames("cursor-pointer flex flex-col justify-between rounded-2xl bg-[#9CD3CD] aspect-square bg-cover border-4 border-[#9CD3CD] hover:scale-105 transition",
-            small ? "h-48 md:h-56 xl:h-64" : "h-56 md:h-64 xl:h-80")}
-             style={{backgroundImage: `url(${img})`}} onClick={onClick}>
-            <div className="text-center py-1 text-xl bg-[#] backdrop-brightness-75 text-white rounded-t-2xl">{name}</div>
-            <button className={classNames("m-2 bg-gradient-to-r from-yellow-400 to-pink-500 self-start rounded-xl text-white", small ? "px-6 py-1" : "px-8 py-2")}>Play</button>
+        <div
+            className={classNames("cursor-pointer flex flex-col justify-between rounded-2xl bg-[#9CD3CD] aspect-square bg-cover border-4 border-[#9CD3CD] hover:scale-105 transition",
+                small ? "h-48 md:h-56 xl:h-64" : "h-56 md:h-64 xl:h-80")}
+            style={{backgroundImage: `url(${img})`}} onClick={onClick}>
+            <div
+                className="text-center py-1 text-xl bg-[#] backdrop-brightness-75 text-white rounded-t-2xl">{name}</div>
+            <Popup trigger={
+                <button
+                    className={classNames("m-2 bg-gradient-to-r from-yellow-400 to-pink-500 self-start rounded-xl text-white", small ? "px-6 py-1" : "px-8 py-2")}>Play
+                </button>}>
+                {close => (
+                    <div className="modal">
+                        <div className="header"><b>Settings Placeholder</b></div>
+                        <div className="content">
+                            {' '}
+                            • Change Setting #1<br/>
+                            • Change Setting #2<br/>
+                            • Change Setting #3<br/>
+                            <button
+                                className={classNames("m-2 bg-gradient-to-r from-yellow-400 to-pink-500 self-start rounded-xl text-white", small ? "px-6 py-1" : "px-8 py-2")}>
+                                Create Game
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+            </Popup>
         </div>
     );
 }
