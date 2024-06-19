15703d9b7f88e8b2d03c1a63b17e0a8d025d1721
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-06-19 03:21:21 +0300

    Add Prometheus support

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.bridge.router import router as bridge_router
 from src.database import init_db
@@ -12,7 +13,12 @@ from src.vars.config import APP_ROOT_PATH, APP_DESCRIPTION, APP_TITLE
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # before startup
+    log.info("Database initialization")
     await init_db()
+
+    log.info("Prometheus instrumentator initialization")
+    instrumentator.expose(app)
+
     log.info("Application startup")
 
     yield
@@ -27,6 +33,7 @@ app = FastAPI(
     title=APP_TITLE,
     description=APP_DESCRIPTION,
 )
+instrumentator = Instrumentator().instrument(app)
 
 # include routers
 app.include_router(bridge_router)
