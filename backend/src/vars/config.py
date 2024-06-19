1b7cec2e632b3a1e95d1ba06bf19a4821caa406e
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-06-19 03:30:36 +0300

    Improve backend (#15)
    * Rename dev.env to .env.sample
    
    * Improve configs
    
    * Improve docs
    
    * Add Prometheus support
    
    * Add init file

--- backend/src/vars/config.py
+++ backend/src/vars/config.py
@@ -1,21 +1,30 @@
-import os
+from os import getenv
 
 from dotenv import load_dotenv
 
 from src.logs.log import log
 
-log.info("Started reading .ENV file")
-load_dotenv("../config/.env")
-try:
-    DB_NAME = os.environ["DATABASE_NAME"]
-    DB_HOST = os.environ["DATABASE_HOST"]
-    TG_BOT_URL = os.environ["TG_BOT_URL"]
-    TOKEN = os.environ["BOT_TOKEN"]
-    FRONT_CREATE_GAME_LINK = os.environ["FRONT_CREATE_GAME_LINK"]
-    FRONT_JOIN_GAME_LINK = os.environ["FRONT_JOIN_GAME_LINK"]
-    AUTH_SECRET = os.environ["AUTH_SECRET"]
+IN_DOCKER = getenv("IN_DOCKER", False)
 
+if not IN_DOCKER:
+    log.info("Application is running locally, reading .env file in config.py")
 
-    log.info("Successfully read .ENV file")
-except Exception as e:
-    log.error(f"Error reading .ENV file {e}")
+    try:
+        load_dotenv("../config/.env")
+        log.info("Successfully read .env file")
+    except Exception as e:
+        log.error(f"Error reading .env file: {e}")
+else:
+    log.info("Application is running in Docker, no need to read .env file in config.py")
+
+APP_ROOT_PATH = "/api"
+APP_TITLE = "Rooma"
+APP_DESCRIPTION = "Online service for offline games"
+
+DB_NAME = getenv("DATABASE_NAME")
+DB_HOST = getenv("DATABASE_HOST")
+TG_BOT_URL = getenv("TG_BOT_URL")
+TOKEN = getenv("BOT_TOKEN")
+FRONT_CREATE_GAME_LINK = getenv("FRONT_CREATE_GAME_LINK")
+FRONT_JOIN_GAME_LINK = getenv("FRONT_JOIN_GAME_LINK")
+AUTH_SECRET = getenv("AUTH_SECRET")
