eb5844dde70179578887a64d770a9a1eb92560fa
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-24 16:40:23 +0300

    Returning None if game state was not found

--- backend/src/common/repository/game_state.py
+++ backend/src/common/repository/game_state.py
@@ -12,10 +12,16 @@ class GameStateRepository(MongoBeanieRepository):
     async def get_by_game_id(self, game_id: UUID) -> GameState | None:
         return await self.model.find_one(Eq(self.model.game_id, game_id))
 
-    async def get_state_by_game_id(self, game_id: UUID) -> str:
+    async def get_state_by_game_id(self, game_id: UUID) -> str | None:
         game_state = await self.get_by_game_id(game_id)
+        if game_state is None:
+            return None
+
         return game_state.state
 
     async def set_state_by_game_id(self, game_id: UUID, state: str):
         game_state = await self.get_by_game_id(game_id)
+        if game_state is None:
+            return
+
         await game_state.update(Set({self.model.state: state}))
