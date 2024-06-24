3dc212cdac9ae2a62abdd800b39587c72ba03f92
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-25 02:42:16 +0300

    Added other hide n seek routes

--- backend/src/games/hide_n_seek/routes.py
+++ backend/src/games/hide_n_seek/routes.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 
 from src.auth.dependencies import get_user
 from src.database.models import User
+from src.games.hide_n_seek.schemas import HidersResponse, EndTimesResponse, StateResponse
 from src.games.hide_n_seek.service import GameService
 from src.schemas import SuccessResponse
 
@@ -22,10 +23,26 @@ service = GameService()
 async def find_hider(code: str, game_id: UUID, user: User = Depends(get_user)):
     return await service.find_hider(code, game_id, user)
 
-# @router.get(
-#
-# )
 
-# get all hiders
-# get times
-# game state
+@router.get(
+    "/{game_id}/hiders",
+    response_model=HidersResponse
+)
+async def get_hiders(game_id: UUID, user: User = Depends(get_user)):
+    return await service.get_hiders(game_id, user)
+
+
+@router.get(
+    "/{game_id}/end-times",
+    response_model=EndTimesResponse
+)
+async def get_end_times(game_id: UUID, user: User = Depends(get_user)):
+    return await service.get_end_time(game_id, user)
+
+
+@router.get(
+    "/{game_id}/state",
+    response_model=StateResponse
+)
+async def get_state(game_id: UUID, user: User = Depends(get_user)):
+    return await service.get_state(game_id, user)
--- backend/src/games/hide_n_seek/service.py
+++ backend/src/games/hide_n_seek/service.py
@@ -4,7 +4,9 @@ from src.common.repository.game import GameRepository
 from src.common.repository.game_state import GameStateRepository
 from src.common.repository.user import UserRepository
 from src.database import User
-from src.games.exceptions import GameForbiddenException, UserNotFoundException
+from src.games.exceptions import GameForbiddenException, UserNotFoundException, GameNotFoundException
+from src.games.hide_n_seek.schemas import HidersResponse, EndTimesResponse, StateResponse
+from src.games.schemas import Player
 from src.schemas import SuccessResponse
 
 game_repo = GameRepository()
@@ -27,3 +29,30 @@ class GameService:
             raise UserNotFoundException(code)
 
         return SuccessResponse(success=True)
+
+    async def get_hiders(self, game_id: UUID, user: User) -> HidersResponse:
+        game = await game_repo.get_one_by_game_id(game_id)
+        if "hiders" not in game.data:
+            raise GameForbiddenException(game_id)
+
+        users = await user_repo.get_users_from_telegram_ids(game.data["hiders"].keys())
+        hiders = [Player(**user.model_dump()) for user in users]
+
+        return HidersResponse(hiders=hiders)
+
+    async def get_end_time(self, game_id: UUID, user: User) -> EndTimesResponse:
+        game = await game_repo.get_one_by_game_id(game_id)
+        if "seeker_start_time" not in game.data or "game_end_time" not in game.data:
+            raise GameForbiddenException(game_id)
+
+        return EndTimesResponse(
+            seeker_start_time=game.data["seeker_start_time"],
+            game_end_time=game.data["game_end_time"]
+        )
+
+    async def get_state(self, game_id: UUID, user: User) -> StateResponse:
+        state = await game_state_repo.get_state_by_game_id(game_id)
+        if state is None:
+            raise GameNotFoundException(game_id)
+
+        return StateResponse(state=state)
