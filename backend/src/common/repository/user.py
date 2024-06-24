5e5ae9f8412a45646bb6c69ce65bcdac7203d00d
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-24 13:21:58 +0300

    Updated user repo

--- backend/src/common/repository/user.py
+++ backend/src/common/repository/user.py
@@ -1,3 +1,5 @@
+from beanie.operators import In, Eq
+
 from src.database.models import User
 from src.database.repository.mongo import MongoBeanieRepository
 
@@ -6,4 +8,7 @@ class UserRepository(MongoBeanieRepository):
     model = User
 
     async def get_user(self, telegram_id: int) -> User | None:
-        return await self.model.find_one(self.model.telegram_id == telegram_id)
+        return await self.model.find_one(Eq(self.model.telegram_id, telegram_id))
+
+    async def get_users_from_telegram_ids(self, telegram_ids: list[int]) -> list[User]:
+        return await self.model.find(In(self.model.telegram_id, telegram_ids)).to_list()
