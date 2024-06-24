b78dd57a81944e0300b67e5a1b886ae74c1e5643
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-24 13:29:08 +0300

    Changed distribute to new logic

--- backend/src/state_manager/hide_n_seek/states/distribute.py
+++ backend/src/state_manager/hide_n_seek/states/distribute.py
@@ -5,39 +5,45 @@ from src.logs.log import log
 from src.state_manager.hide_n_seek.handler import StateHandler
 from src.state_manager.hide_n_seek.state import State
 
-MIN_TO_START = 2
 CODE_LENGTH = 6
 
 
 class StateHandlerDistribute(StateHandler):
     async def handle(self):
+        # fetch our game and check if it exists
         game = await self.get_current_game()
         if game is None:
             log.error(f"Cannot find game with id = {self.game_id}")
             return
 
+        # get seeker percentage and check that it exists in data
         seeker_percentage = game.data.get("seeker_percentage")
         if seeker_percentage is None:
             log.error(f"Seeker percentage is not present in game with id = {self.game_id}")
             return
 
-        if len(game.lobby) < MIN_TO_START:
+        # cannot distribute 0 or 1 people into two teams
+        if len(game.lobby) < 2:
             log.error(f"Not enough people to distribute in game with id = {self.game_id}")
             return
 
+        # calculate seeker size and check if there are no seekers or hiders
         seeker_size = int(seeker_percentage / 100) * len(game.lobby)
         if seeker_size == 0:  # no seekers
             seeker_size = 1
         elif seeker_size == len(game.lobby):  # no hiders
             seeker_size = len(game.lobby) - 1
 
+        # distribute
         seekers = set(sample(game.lobby, seeker_size))
         hiders = set(game.lobby) - seekers
 
+        # generate codes
         codes = generate_unique_codes(amount=len(hiders), length=CODE_LENGTH)
 
-        game.data["hiders"] = {user.telegram_id: code for user, code in zip(hiders, codes)}
-
+        # update data about teams
+        game.data["hiders"] = {telegram_id: code for telegram_id, code in zip(hiders, codes)}
         _ = await game.save()
 
+        # go to new state
         await self.set_new_state(State.START)
