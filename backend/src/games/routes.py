87dde8bc9c0af980243187ea7a3c10ad8cd40f80
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-25 00:14:45 +0300

    Fixed auth

--- backend/src/games/routes.py
+++ backend/src/games/routes.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends
 
-from src.auth.dependencies import verify_token
+from src.auth.dependencies import get_user
 from src.common.repository.game import GameRepository
 from src.common.repository.user import UserRepository
 from src.database.models import User
@@ -18,8 +18,7 @@ from src.schemas import SuccessResponse
 
 router = APIRouter(
     prefix="/games",
-    tags=["Games"],
-    dependencies=[Depends(verify_token)],
+    tags=["Games"]
 )
 
 game_repo = GameRepository()
@@ -32,7 +31,7 @@ service = GameService()
     "/create",
     response_model=Game,
 )
-async def create_game(data: CreateGameDTO, user: User = Depends()):
+async def create_game(data: CreateGameDTO, user: User = Depends(get_user)):
     return await service.create_game(data, user)
 
 
@@ -40,7 +39,7 @@ async def create_game(data: CreateGameDTO, user: User = Depends()):
     "/{game_id}/join",
     response_model=SuccessResponse,
 )
-async def join_game(game_id: UUID, user: User = Depends()):
+async def join_game(game_id: UUID, user: User = Depends(get_user)):
     return await service.join_game(game_id, user)
 
 
@@ -48,7 +47,7 @@ async def join_game(game_id: UUID, user: User = Depends()):
     "/{game_id}/leave",
     response_model=SuccessResponse,
 )
-async def leave_game(game_id: UUID, user: User = Depends()):
+async def leave_game(game_id: UUID, user: User = Depends(get_user)):
     return await service.leave_game(game_id, user)
 
 
@@ -56,7 +55,7 @@ async def leave_game(game_id: UUID, user: User = Depends()):
     "/{game_id}/lobby",
     response_model=LobbyResponse,
 )
-async def get_lobby(game_id: UUID, user: User = Depends()):
+async def get_lobby(game_id: UUID, user: User = Depends(get_user)):
     return await service.get_lobby(game_id, user)
 
 
@@ -64,7 +63,7 @@ async def get_lobby(game_id: UUID, user: User = Depends()):
     "/{game_id}/start",
     response_model=SuccessResponse,
 )
-async def start_game(game_id: UUID, user: User = Depends()):
+async def start_game(game_id: UUID, user: User = Depends(get_user)):
     return await service.start_game(game_id, user)
 
 
@@ -72,7 +71,7 @@ async def start_game(game_id: UUID, user: User = Depends()):
     "/{game_id}/finish",
     response_model=SuccessResponse,
 )
-async def finish_game(game_id: UUID, user: User = Depends()):
+async def finish_game(game_id: UUID, user: User = Depends(get_user)):
     return await service.finish_game(game_id, user)
 
 
@@ -80,7 +79,7 @@ async def finish_game(game_id: UUID, user: User = Depends()):
     "/{game_id}/rules",
     response_model=RulesResponse,
 )
-async def get_rules(game_id: UUID, user: User = Depends()):
+async def get_rules(game_id: UUID, user: User = Depends(get_user)):
     return await service.get_rules(game_id, user)
 
 
@@ -88,7 +87,7 @@ async def get_rules(game_id: UUID, user: User = Depends()):
     "/{game_id}",
     response_model=Game,
 )
-async def get_game(game_id: UUID, user: User = Depends()):
+async def get_game(game_id: UUID, user: User = Depends(get_user)):
     return await service.get_game(game_id, user)
 
 
@@ -96,59 +95,5 @@ async def get_game(game_id: UUID, user: User = Depends()):
     "/{game_id}/feedback",
     response_model=SuccessResponse,
 )
-async def post_feedback(data: PostFeedbackDTO, game_id: UUID, user: User = Depends()):
+async def post_feedback(data: PostFeedbackDTO, game_id: UUID, user: User = Depends(get_user)):
     return await service.post_feedback(data, game_id, user)
-
-
-# @router.post(
-#     "/create",
-#     response_model=GetGameLinkResponseSchema,
-#     responses={status.HTTP_403_FORBIDDEN: {"model": ErrorSchema}},
-#
-# )
-# async def create_game(token: str, telegram_id: int, game_data: CreateGameSchema):
-#     """
-#     Creates a game object in the database and returns a link to Telegram for token validation.
-#
-#     If token is invalid, then 403 is returned.
-#     """
-#     created = await game_repo.create_one(game_data)
-#     log.info(f"Create game with id = {created.game_id}")
-#     return GetGameLinkResponseSchema(link=f"{TG_BOT_URL}{created.game_id}")
-#
-#
-# @router.post(
-#     "/join",
-#     responses={status.HTTP_403_FORBIDDEN: {"model": ErrorSchema},
-#                status.HTTP_404_NOT_FOUND: {"model": ErrorSchema}},
-#     dependencies=[Depends(verify_token)]
-# )
-# async def join_game(token: str, telegram_id: int, game_id: UUID):
-#     """
-#     Joins a user to the game by its `game_id`. The user is added to lobby field.
-#
-#     If user is not found, then 404 is returned.\\
-#     If token is invalid, then 403 is returned.
-#     """
-#     user = await user_repo.get_user(telegram_id)
-#     if user is None:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail="Could not find user"
-#         )
-#     await game_repo.add_user_to_lobby(game_id, user)
-#
-#
-# @router.get(
-#     "/lobby",
-#     response_model=list[User],
-#     responses={status.HTTP_403_FORBIDDEN: {"model": ErrorSchema}},
-#     dependencies=[Depends(verify_token)]
-# )
-# async def get_all_users(token: str, telegram_id: int, game_id: UUID):
-#     """
-#     Returns all users in the game lobby by its `game_id`.
-#
-#     If token is invalid, then 403 is returned.
-#     """
-#     return await game_repo.get_all_users_from_lobby(game_id)
