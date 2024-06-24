9d3cc8b57b52b9af4f15e30ee517b747989eeb94
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-24 16:47:43 +0300

    Adding logic for handler.py for new requests

--- backend/src/state_manager/hide_n_seek/handler.py
+++ backend/src/state_manager/hide_n_seek/handler.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from src.common.repository.game import GameRepository
 from src.common.repository.game_state import GameStateRepository
+from src.common.schemas import CreateGameStateSchema
 from src.database import Game
 from src.state_manager.hide_n_seek.state import State
 
@@ -10,6 +11,8 @@ game_state_repo = GameStateRepository()
 
 
 class StateHandler:
+    __entry_state = State.DISTRIBUTE
+
     def __init__(self, game_id: UUID):
         self.game_id = game_id
 
@@ -26,19 +29,34 @@ class StateHandler:
         current_state_handler = await self.get_current_state_handler()
         await current_state_handler.handle()
 
-    async def get_current_state_handler(self):
+    async def get_current_state(self) -> State:
         state = await game_state_repo.get_state_by_game_id(self.game_id)
+        if state is not None:
+            return State(state)
+
+        await game_state_repo.create_one(
+            CreateGameStateSchema(
+                game_id=self.game_id,
+                state=self.__entry_state
+            )
+        )
+
+        return self.__entry_state
+
+    async def get_current_state_handler(self):
+        state = await self.get_current_state()
         handler = self.get_handler_by_state(State(state))
         return handler
 
     def get_handler_by_state(self, state: State):
         from src.state_manager.hide_n_seek.states.distribute import StateHandlerDistribute
         from src.state_manager.hide_n_seek.states.start import StateHandlerStart
+        from src.state_manager.hide_n_seek.states.hiding import StateHandlerHiding
 
         state_to_handler = {
             State.DISTRIBUTE: StateHandlerDistribute,
             State.START: StateHandlerStart,
-            # None: StateHandlerArchived
+            State.HIDING: StateHandlerHiding
         }
 
         return state_to_handler[state](self.game_id)
