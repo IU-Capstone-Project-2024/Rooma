66a7b2714c42ca2fc01f65aecf8c805fa5a13439
darya <d.koncheva@innopolis.university>
2024-06-26 02:17:07 +0300

    leave game and get lobby

--- frontend/src/api/games.js
+++ frontend/src/api/games.js
@@ -41,3 +41,32 @@ export const joinGame = async (gameId) => {
         throw error;
     }
 }
+
+export const leaveGame = async (gameId) => {
+    const url = `${GAMES_URL}/${gameId}/leave?token=${token}`;
+
+    try {
+        const response = await axios.post(url);
+        console.log('Game leaved:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error leaving game:', error);
+        throw error;
+    }
+}
+
+export const getLobby = async (gameId) => {
+    const url = `${GAMES_URL}/${gameId}/lobby?token=${token}`;
+
+    try {
+        const response = await axios.get(url);
+        return {
+            lobby: response.data
+        };
+    } catch (error) {
+        console.error('Error getting lobby:', error);
+        throw error;
+    }
+}
+
+
