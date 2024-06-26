ab0cfd0819608adb0575c876e05fa199384a4088
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-26 12:38:10 +0300

    Removed JoinGame

--- frontend/src/pages/JoinGame/WaitPage.jsx
+++ frontend/src/pages/JoinGame/WaitPage.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {joinGame} from "@/api/gamesCommon.js";
+import {getDuration} from "@/api/hideAndSeek.js";
 
 export default function WaitPage() {
-    // TODO: add states etc. to prevent double execution
+    const [duration, setDuration] = useState(null);
+    const [waitTime, setWaitTime] = useState(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const navigate = useNavigate();
+
+    const game_id = searchParams.get("game_id");
+
+    useEffect(() => {
+        if (!game_id) {
+            navigate("/");
+        }
+    }, []);
+
+    joinGame(game_id);
+
+    useEffect(() => {
+        getDuration(game_id).then((result) => {
+            setDuration(result["duration"]);
+            setWaitTime(result["time_to_hide"]);
+        });
+    }, []);
+
     return (
         <section className="bg-[#FF7F29] space-y-10">
             <h1 className="text-xl text-white font-bold mb-2">Hide and seek</h1>
 
-            <div className="my-8 mx-10 sm:mx-20 md:mx-28 lg:mx-36 text-center text-lg space-y-4 border-4 border-t-amber-400">
+            <div
+                className="my-8 mx-10 sm:mx-20 md:mx-28 lg:mx-36 text-center text-lg space-y-4 border-4 border-t-amber-400">
 
                 <div className="flex items-center justify-between">
                     <p>Number of participants:</p>
@@ -18,14 +44,14 @@ export default function WaitPage() {
                 <div className="flex items-center justify-between">
                     <p>Game time:</p>
                     <div className="bg-[#FFC87A] px-2 rounded-lg">
-                        <p>1:30:00</p>
+                        <p>{duration} minutes</p>
                     </div>
                 </div>
 
                 <div className="flex items-center justify-between">
-                    <p>Game time:</p>
+                    <p>Waiting time for seekers:</p>
                     <div className="bg-[#FFC87A] px-2 rounded-lg">
-                        <p>00:10:00</p>
+                        <p>{waitTime} minutes</p>
                     </div>
                 </div>
 
