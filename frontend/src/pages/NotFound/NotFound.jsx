213c642569908726ab92b75acf8ce3963029d42c
darya <d.koncheva@innopolis.university>
2024-06-26 12:59:48 +0300

    add not found styles

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <>
-            <h1>404</h1>
-            <h2>Page not found</h2>
-            <Link to={"/"}>Go to main page</Link>
-        </>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#3FC7B8]">
+            <h1 className="text-9xl font-bold text-white mb-4">404</h1>
+            <h2 className="text-2xl font-semibold text-white mb-8">Page not found</h2>
+            <Link to="/">
+                <button className="px-8 py-4 text-lg bg-white text-black font-semibold rounded-lg shadow-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    Go to main page
+                </button>
+            </Link>
+        </div>
     );
 };
 
