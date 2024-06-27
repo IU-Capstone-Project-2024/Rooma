687f475fa7696982aa0f8edd8d9bf9fd7f8e61da
darya <d.koncheva@innopolis.university>
2024-06-27 14:47:57 +0300

    add nav to main from logo

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,15 +1,23 @@
 import logo from "../../assets/logo.svg";
 import {useAuth} from '@/components/business/useAuth';
+import {useNavigate} from "react-router-dom";
 
 export default function Header() {
     const {signOut} = useAuth();
+    const navigate = useNavigate();
+
+    const handleNavigateToMain = () => {
+        navigate('/');
+    };
 
     return (
         <header className="flex w-full h-24 px-12 justify-between items-center">
-            <div className="flex items-center gap-5">
-                <img src={logo} alt="logo" className="h-14"/>
-                <span className="text-white text-3xl hidden sm:block">Rooma</span>
-            </div>
+            <button onClick={handleNavigateToMain}>
+                <div className="flex items-center gap-5">
+                    <img src={logo} alt="logo" className="h-14"/>
+                    <span className="text-white text-3xl hidden sm:block">Rooma</span>
+                </div>
+            </button>
 
             <button
                 className="bg-[#9CD3CD72] h-14 px-5 text-white text-lg rounded-full"
