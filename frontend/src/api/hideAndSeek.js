3e214ab3bf31852afcd8d8b061f1757d37210729
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-30 13:02:38 +0300

    Updated frontend to use new api

--- frontend/src/api/hideAndSeek.js
+++ frontend/src/api/hideAndSeek.js
@@ -3,6 +3,28 @@ import {BASE_URL} from "@/constants/urls.js";
 
 const GAMES_URL = BASE_URL + '/api/games/hide-n-seek';
 
+export const createGame = async ( name, duration, timeToHide, seekerPercentage) => {
+    const url = `${GAMES_URL}/create?token=${localStorage.getItem("token")}`;
+
+    const gameInfo = {
+        name: name,
+        data: {
+            duration: duration,
+            time_to_hide: timeToHide,
+            seeker_percentage: seekerPercentage
+        }
+    };
+
+    try {
+        const response = await axios.post(url, gameInfo);
+        console.log('Game created:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating game:', error);
+        throw error; // Пробрасываем ошибку дальше, если нужно обработать в другом месте
+    }
+};
+
 export const getDuration = async (gameId) => {
     const url = `${GAMES_URL}/${gameId}/durations?token=${localStorage.getItem("token")}`;
 
