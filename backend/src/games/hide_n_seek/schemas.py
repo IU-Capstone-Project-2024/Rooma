8f0294ce4ffb646dce729ae89eed12f701090482
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-30 11:17:35 +0300

    Added schema for creating hide_n_seek

--- backend/src/games/hide_n_seek/schemas.py
+++ backend/src/games/hide_n_seek/schemas.py
@@ -1,8 +1,25 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.games.schemas import Player
+
+
+class HideNSeekData(BaseModel):
+    seeker_percentage: int = Field(..., ge=0, le=100)
+    duration: int = Field(..., ge=0)  # minutes
+    time_to_hide: int = Field(..., ge=0)  # minutes
+
+    # set by backend
+    hiders: dict[int, str] | None = None  # dict[telegram_id, code]
+    hiders_found: list[int] | None = None  # list[telegram_id]
+    seeker_start_time: datetime | None = None
+    game_end_time: datetime | None = None
+
+
+class CreateHideNSeekRequest(BaseModel):
+    name: str
+    data: HideNSeekData
 
 
 class HidersResponse(BaseModel):
