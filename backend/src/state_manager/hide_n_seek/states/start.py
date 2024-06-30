c23911c2d8cedd5d347359c080b92f5492fcb30e
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-06-30 12:05:32 +0300

    Changed start to new schema

--- backend/src/state_manager/hide_n_seek/states/start.py
+++ backend/src/state_manager/hide_n_seek/states/start.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from src.games.hide_n_seek.schemas import HideNSeekData
 from src.logs.log import log
 from src.state_manager.hide_n_seek.handler import StateHandler
 from src.state_manager.hide_n_seek.state import State
@@ -14,31 +15,18 @@ class StateHandlerStart(StateHandler):
         if game is None:
             return
 
-        # get duration and check that it exists in data
-        duration = game.data.get("duration")
-        if duration is None:
-            log.error(f"Duration is not present in game with id = {self.game_id}")
-            return
-
-        # get time to hide and check that it exists in data
-        time_to_hide = game.data.get("time_to_hide")
-        if time_to_hide is None:
-            log.error(f"Time to hide is not present in game with id = {self.game_id}")
-            return
-
-        # game ends before seekers start
-        if time_to_hide >= duration:
-            log.error(f"Hiding takes more time than duration in game with id = {self.game_id}")
-            return
+        data = HideNSeekData(**game.data)
 
         # calculate finish times
         current_time = datetime.utcnow()
-        seeker_start_time = current_time + timedelta(minutes=time_to_hide)
-        game_end_time = current_time + timedelta(minutes=duration)
+        seeker_start_time = current_time + timedelta(minutes=data.time_to_hide)
+        game_end_time = current_time + timedelta(minutes=data.duration)
 
         # save end times
-        game.data["seeker_start_time"] = seeker_start_time
-        game.data["game_end_time"] = game_end_time
+        data.seeker_start_time = seeker_start_time
+        data.game_end_time = game_end_time
+
+        game.data = data.model_dump()
         _ = await game.save()
 
         # go to new state
