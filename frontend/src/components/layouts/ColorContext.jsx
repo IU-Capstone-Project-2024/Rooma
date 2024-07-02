b2a3f165015332c3b72e4e81b2fd20ae428596a3
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-07-03 00:44:29 +0300

    Added ColorContext.jsx

---
+++ frontend/src/components/layouts/ColorContext.jsx
@@ -0,0 +1,23 @@
+// ColorContext.jsx
+import React, {createContext, useContext, useState} from 'react';
+
+const ColorContext = createContext();
+
+export const ColorProvider = ({children}) => {
+    const [headerColor, setHeaderColor] = useState('#3FC7B8');
+    const [footerColor, setFooterColor] = useState('#3FC7B8');
+    const [backgroundColor, setBackgroundColor] = useState('#3FC7B8');
+
+    const value = {
+        headerColor, footerColor, backgroundColor,
+        setHeaderColor, setFooterColor, setBackgroundColor
+    }
+
+    return (
+        <ColorContext.Provider value={value}>
+            {children}
+        </ColorContext.Provider>
+    );
+};
+
+export const useColor = () => useContext(ColorContext);
