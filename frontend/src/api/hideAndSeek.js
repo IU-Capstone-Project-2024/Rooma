2ec3cd5789f3363d7351cf24009ee0b9adc4e3bd
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-03 12:12:19 +0300

    Add function for getting end times

--- frontend/src/api/hideAndSeek.js
+++ frontend/src/api/hideAndSeek.js
@@ -73,3 +73,15 @@ export const getSeekerResults = async (gameId) => {
         throw error;
     }
 }
+
+export const getEndTimes = async (gameId) => {
+    const url = `${GAMES_URL}/${gameId}/end-times?token=${localStorage.getItem("token")}`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error getting end times:', error);
+        throw error;
+    }
+}
