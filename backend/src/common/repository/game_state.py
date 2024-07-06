e07de6d6f4028a68d910f7985f6c0c3d59cb68d6
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-07-06 17:25:47 +0300

    Added some additional methods for game state

--- backend/src/common/repository/game_state.py
+++ backend/src/common/repository/game_state.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from beanie.operators import Eq, Set
+from beanie.operators import Eq, Set, NotIn
 
 from src.database.models import GameState
 from src.database.repository.mongo import MongoBeanieRepository
@@ -11,6 +11,12 @@ class GameStateRepository(MongoBeanieRepository):
 
     async def get_by_game_id(self, game_id: UUID) -> GameState | None:
         return await self.model.find_one(Eq(self.model.game_id, game_id))
+
+    async def get_not_in_states(self, state: list[str]) -> list[GameState]:
+        return await self.model.find(NotIn(self.model.state, state)).to_list()
+
+    async def get_by_state(self, state: str) -> list[GameState]:
+        return await self.model.find(Eq(self.model.state, state)).to_list()
 
     async def get_state_by_game_id(self, game_id: UUID) -> str | None:
         game_state = await self.get_by_game_id(game_id)
