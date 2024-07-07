d193fa5cab7f549cff22145ba7da36d9600cbec5
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-07-07 23:06:23 +0300

    Returned is none back

--- backend/src/common/repository/game_state.py
+++ backend/src/common/repository/game_state.py
@@ -4,18 +4,13 @@ from beanie.operators import Eq, Set, NotIn
 
 from src.database.models import GameState
 from src.database.repository.mongo import MongoBeanieRepository
-from src.exceptions import NotFoundException
 
 
 class GameStateRepository(MongoBeanieRepository):
     model = GameState
 
-    async def get_by_game_id(self, game_id: UUID) -> GameState:
-        state = await self.model.find_one(Eq(self.model.game_id, game_id))
-        if state is None:
-            raise NotFoundException("State")
-
-        return state
+    async def get_by_game_id(self, game_id: UUID) -> GameState | None:
+        return await self.model.find_one(Eq(self.model.game_id, game_id))
 
     async def get_not_in_states(self, states: list[str]) -> list[GameState]:
         return await self.model.find(NotIn(self.model.state, states)).to_list()
@@ -25,10 +20,14 @@ class GameStateRepository(MongoBeanieRepository):
 
     async def get_state_by_game_id(self, game_id: UUID) -> str | None:
         game_state = await self.get_by_game_id(game_id)
+        if game_state is None:
+            return None
 
         return game_state.state
 
     async def set_state_by_game_id(self, game_id: UUID, state: str):
         game_state = await self.get_by_game_id(game_id)
+        if game_state is None:
+            return None
 
         await game_state.update(Set({self.model.state: state}))
