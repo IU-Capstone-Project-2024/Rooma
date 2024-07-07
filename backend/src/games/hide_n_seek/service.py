2d9ca0b210e32ac5ce6b7a335dc9bf286cd25599
Dmitrii Okoneshnikov <magicwinnie@yandex.ru>
2024-07-07 23:06:49 +0300

    Updated if

--- backend/src/games/hide_n_seek/service.py
+++ backend/src/games/hide_n_seek/service.py
@@ -172,7 +172,7 @@ class GameService:
     async def get_role(self, game_id: UUID, telegram_id: int) -> PlayerRoleResponse:
         game = await game_repo.get_one_by_game_id(game_id)
         state = await game_state_repo.get_by_game_id(game.game_id)
-        if not game.is_active or state == State.START:
+        if not game.is_active or state is None or state == State.START:
             raise GameForbiddenException(game_id)
 
         data = HideNSeekData(**game.data)
